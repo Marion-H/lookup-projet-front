730d02d8557ea9a127c3b5e68101ab2a9fea9b9c
Marion-H <doudou6500@gmail.com>
2021-01-12 16:35:17 +0100

    update script

--- src/apiUrl.js
+++ src/apiUrl.js
@@ -1,3 +1,13 @@
+// let apiUrl;
+
+// if (process.env.NODE_ENV === 'development') {
+//   apiUrl = 'http://localhost:8000';
+// } else {
+//   apiUrl = 'https://back-lookup.herokuapp.com/';
+// }
+
+// export default apiUrl;
+
 const apiUrl = 'http://localhost:8000';
 
 export default apiUrl;
