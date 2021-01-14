61316928b572a761c788de29972a0001b38885b1
Marion-H <doudou6500@gmail.com>
2021-01-14 13:07:32 +0100

    update api url

--- src/apiUrl.js
+++ src/apiUrl.js
@@ -1,13 +1,14 @@
-// let apiUrl;
+// eslint-disable-next-line import/no-mutable-exports
+let apiUrl;
 
-// if (process.env.NODE_ENV === 'development') {
-//   apiUrl = 'http://localhost:8000';
-// } else {
-//   apiUrl = 'https://back-lookup.herokuapp.com';
-// }
-
-// export default apiUrl;
-
-const apiUrl = 'https://back-lookup.herokuapp.com';
+if (process.env.NODE_ENV === 'development') {
+  apiUrl = 'http://localhost:8000';
+} else {
+  apiUrl = 'https://back-lookup.herokuapp.com';
+}
 
 export default apiUrl;
+
+// const apiUrl = 'https://back-lookup.herokuapp.com';
+
+// export default apiUrl;
